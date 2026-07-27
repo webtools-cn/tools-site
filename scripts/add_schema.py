3a0a07ab1dbe0b00b7729d9d37253a2c57047d92
WebTools CN <shexiangsheng@gmail.com>
2026-07-27 20:41:21 +0800

    chore: 质量闭环自动修复 - 本轮9个问题 (schema/js_alert等)

--- scripts/add_schema.py
+++ scripts/add_schema.py
@@ -23,9 +23,15 @@ for path, name, desc in FILES_DATA:
     
     # 在 AdSense 脚本后插入
     schema = schema_tpl.format(name=name, desc=desc, path=path)
-    marker = 'crossorigin="anonymous"></script>'
-    if marker in content:
-        content = content.replace(marker, marker + '\n' + schema)
+    # 尝试多种marker
+    markers = ['crossorigin="anonymous"></script>', 'crossorigin="anonymous">\n']
+    for marker in markers:
+        if marker in content:
+            content = content.replace(marker, marker + '\n' + schema)
+            break
+    else:
+        # fallback: 在</head>之前插入
+        content = content.replace('</head>', schema + '\n</head>')
     
     with open(f, 'w') as fh:
         fh.write(content)
